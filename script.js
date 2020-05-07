d1383f3dbde85f13d0951ecb51bef9592749252a
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-05-06 21:49:24 -0500

    function is working

--- script.js
+++ script.js
@@ -13,34 +13,33 @@ var charSpecial = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 var userInput = prompt(
   "Please select a passweod length? (Between 8-128 characters)"
 );
-for (var i = 0; i < userInput; i++) {
-  var num = Math.floor(Math.random() * 10) + 1;
-  console.log(num);
-}
 
 var upperCase = confirm("Do you want to include upper case letters?");
-
 if (upperCase === false) {
   charSet = charSet.toLowerCase();
   var userGuessUpper = charSet[Math.floor(Math.random() * charSet.length)];
-  console.log(userGuessUpper);
 } else {
   var userGuessLower = charSet[Math.floor(Math.random() * charSet.length)];
-  console.log(userGuessLower);
 }
 
 var specialCharacters = confirm("Do want to include special characters?");
 if (specialCharacters === true) {
   var userSpSel = charSpecial[Math.floor(Math.random() * charSpecial.length)];
-
-  console.log(userSpSel);
 }
 
 var numbers = confirm("Do you want to inlucde numbers?");
 if (numbers === true) {
   var userNum = numSet[Math.floor(Math.random() * numSet.length)];
-  console.log(userNum);
 }
+
+function characterGen() {
+  //console.log(userGuessLower || userGuessUpper, userSpSel, userNum);
+  for (var i = 0; i < characterGen; i++) {
+    var instance = [i(Math.floor(Math.random() * characterGen.length))];
+    console.log(instance);
+  }
+}
+characterGen();
 
 var generateBtn = document.querySelector("#generate");
 
